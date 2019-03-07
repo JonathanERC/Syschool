102736995bae55b7d2baae817c7ae8682137ab4c
JonathanERC <jonathanestebanrondon@gmail.com>
2019-03-07 02:33:54 -0800

    syschool

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapaDatos
+{
+    class Conexion
+    {
+        public static string Cn = "Data Source=JR-LAP-00; Initial Catalog=Prueba; Integrated Security=true";
+    }
+}
