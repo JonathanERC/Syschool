121bf0fe3417e4c4ec5a48f60b25327139f9e909
JonathanERC <jonathanestebanrondon@gmail.com>
2019-04-27 17:59:30 -0700

    syschool

--- SYSCHOOL/Program.cs
+++ SYSCHOOL/Program.cs
@@ -19,7 +19,7 @@ namespace SYSCHOOL
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
-            Application.Run(new frmCursos());
+            Application.Run(new frmAsignaturas());
         }
     }
 }
